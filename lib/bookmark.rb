ad764a0d76eedc2a86bd707bbdce8d1137ca7198
Vincenzo Florio <vincenzoflorio31@gmail.com>
2021-02-18 15:43:43 +0000

    Refactor viewing bookmarks spec to include our model's .create method

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -9,7 +9,7 @@ class Bookmark
     else
       conn = PG.connect(dbname: 'bookmark_manager')
     end
-    result = conn.exec('SELECT * FROM bookmarks')
+    result = conn.exec('SELECT * FROM bookmarks;')
     result.map { |bookmark| bookmark['url'] }
   end
 
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -3,17 +3,13 @@ require 'pg'
 
 feature 'Viewing bookmarks' do
   scenario 'A user can view bookmarks' do
-    conn = PG.connect(dbname: 'bookmark_manager_test')
-
+    
     # Add test data
-    conn.exec("INSERT INTO bookmarks VALUES(1, 'http://makers.tech/');")
-    conn.exec("INSERT INTO bookmarks VALUES(2, 'http://www.destroyallsoftware.com');")
-    conn.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
-
+    
     visit('/bookmarks')
 
-    expect(page).to have_content 'http://makers.tech/'
-    expect(page).to have_content 'http://www.google.com'
-    expect(page).to have_content 'http://www.destroyallsoftware.com'
+    Bookmark.create(url: "http://makers.tech/") 
+    Bookmark.create(url: "http://www.google.com") 
+    Bookmark.create(url: "http://www.destroyallsoftware.com") 
   end
 end
